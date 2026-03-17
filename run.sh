99573125215da935c2ec6dfb44c6cd58a5dfa9dc
Siddharth Chilamkur <schilamkur@berkeley.edu>
2026-03-16 17:45:39 -0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cat src/RAG/eecs_ind.faiss.part.* > src/RAG/eecs_ind.faiss
+cat src/RAG/data_storage.json.part.* > src/RAG/data_storage.json
 
-python3 ./src/RAG/generate.py "$1" "$2"
+python3 src/RAG/rag.py "$1" "$2"
