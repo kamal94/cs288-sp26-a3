5712f9b451d67350d910ad1eca0f8a3d0cddd804
schilamkur <91044775+schilamkur@users.noreply.github.com>
2026-03-17 12:38:50 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 cat src/RAG/eecs_ind.faiss.part.* > src/RAG/eecs_ind.faiss
 cat src/RAG/data_storage.json.part.* > src/RAG/data_storage.json
 
-python3 src/RAG/rag.py "$1" "$2"
+python3 src/RAG/generate.py "$1" "$2"
